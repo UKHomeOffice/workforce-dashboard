ccb9b27306f05f72256bd5c7055286bea9acaf4c
Sharon Briscoe <sharon.briscoe@digital.homeoffice.gov.uk>
2019-06-27 11:49:23 +0100

    Spring setup

---
+++ workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/WorkforceDashboardApplication.java
@@ -0,0 +1,13 @@
+package gov.homeoffice.workforceDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceDashboardApplication.class, args);
+	}
+
+}
