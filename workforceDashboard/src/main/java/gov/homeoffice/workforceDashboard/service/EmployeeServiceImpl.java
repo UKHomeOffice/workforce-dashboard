eb696da42f621013d4ed2cd1d38733332247d362
Sharon Briscoe <sharon.briscoe@digital.homeoffice.gov.uk>
2019-07-04 13:45:57 +0100

    Incorrect Transactional bean remaoved from impl

--- workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/service/EmployeeServiceImpl.java
+++ workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/service/EmployeeServiceImpl.java
@@ -4,12 +4,11 @@ import gov.homeoffice.workforceDashboard.model.Employee;
 import gov.homeoffice.workforceDashboard.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional
+
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
