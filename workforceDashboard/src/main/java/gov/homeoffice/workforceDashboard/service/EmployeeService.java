aae3c2c804ac6220429337b3dfcac957501d9fb9
Sharon Briscoe <sharon.briscoe@digital.homeoffice.gov.uk>
2019-07-04 13:19:15 +0100

    Amended Impl with Transactional bean

--- workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/service/EmployeeService.java
+++ workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/service/EmployeeService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public interface EmployeeService {
 
-    void save(Employee employee);
+    void saveEmployee(Employee employee);
 
     List<Employee> findAll();
 }
--- workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/service/EmployeeServiceImpl.java
+++ workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/service/EmployeeServiceImpl.java
@@ -4,21 +4,23 @@ import gov.homeoffice.workforceDashboard.model.Employee;
 import gov.homeoffice.workforceDashboard.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @Override
-    public void save (Employee employee) {
+
+    public void saveEmployee (Employee employee) {
         employeeRepository.save(employee);
     }
 
-    @Override
+
     public List<Employee> findAll() {
         return employeeRepository.findAll();
     }
