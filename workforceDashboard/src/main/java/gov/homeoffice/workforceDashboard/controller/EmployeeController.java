abd676904039840bf1f1faf2a07bda54e98b66e4
Sharon Briscoe <sharon.briscoe@digital.homeoffice.gov.uk>
2019-07-05 11:07:35 +0100

    Added list requet mapping to controller

--- workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/controller/EmployeeController.java
+++ workforceDashboard/src/main/java/gov/homeoffice/workforceDashboard/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package gov.homeoffice.workforceDashboard.controller;
 
+import gov.homeoffice.workforceDashboard.model.Employee;
 import gov.homeoffice.workforceDashboard.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -14,5 +17,11 @@ public class EmployeeController {
     @RequestMapping("/")
     public String getIntro() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/list")
+    public List<Employee> getEmployeeList() {
+
+        return employeeService.findAll();
     }
 }
